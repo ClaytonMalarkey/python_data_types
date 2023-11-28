8fcfb6f1f01d6aedcd57768a3af701cdec3048f3
ClaytonMalarkey <malarkey1985@gmail.com>
2023-11-27 19:41:43 -0700

    first

---
+++ data_types.py
@@ -0,0 +1,16 @@
+# data_types.py
+
+# Create two string variables
+string1 = "example_test"
+string2 = "another_string"
+
+# Write a conditional block using endswith function
+if string1.endswith('test'):
+    print('Found test in string1')
+else:
+    print('No test in string1')
+
+if string2.endswith('test'):
+    print('Found test in string2')
+else:
+    print('No test in string2')
